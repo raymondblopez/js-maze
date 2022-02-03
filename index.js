201a47c9cb4a850718021fd6dadd0e9affb8bf10
Raymond Lopez <raymondblopez97@gmail.com>
2022-02-03 10:08:31 -0500

    add win message

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const { Engine, Render, Runner, World, Bodies, Body, Events } = Matter;
 
-const cellsHorizontal = 4;
-const cellsVertical = 3;
-const width = window.innerWidth;
-const height = window.innerHeight;
+const cellsHorizontal = 14;
+const cellsVertical = 10;
+const width = window.innerWidth - 4;
+const height = window.innerHeight - 4;
 
 const unitLengthX = width / cellsHorizontal;
 const unitLengthY = height / cellsVertical;
@@ -15,7 +15,7 @@ const render = Render.create({
   element: document.body,
   engine: engine,
   options: {
-    wireframes: true,
+    wireframes: false,
     width,
     height,
   },
@@ -135,6 +135,9 @@ horizontals.forEach((row, rowIndex) => {
       {
         label: 'wall',
         isStatic: true,
+        render: {
+          fillStyle: 'red',
+        },
       }
     );
     World.add(world, wall);
@@ -154,6 +157,9 @@ verticals.forEach((row, rowIndex) => {
       {
         label: 'wall',
         isStatic: true,
+        render: {
+          fillStyle: 'red',
+        },
       }
     );
     World.add(world, wall);
@@ -169,6 +175,9 @@ const goal = Bodies.rectangle(
   {
     label: 'goal',
     isStatic: true,
+    render: {
+      fillStyle: 'green',
+    },
   }
 );
 
@@ -178,6 +187,9 @@ World.add(world, goal);
 const ballRadius = Math.min(unitLengthX, unitLengthY) / 4;
 const ball = Bodies.circle(unitLengthX / 2, unitLengthY / 2, ballRadius, {
   label: 'ball',
+  render: {
+    fillStyle: 'lightblue',
+  },
 });
 World.add(world, ball);
 
@@ -210,6 +222,7 @@ Events.on(engine, 'collisionStart', (event) => {
       labels.includes(collision.bodyA.label) &&
       labels.includes(collision.bodyB.label)
     ) {
+      document.querySelector('.winner').classList.remove('hidden');
       world.gravity.y = 1;
       world.bodies.forEach((body) => {
         if (body.label === 'wall') {
